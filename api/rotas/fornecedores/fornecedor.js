bbbd1905e3e08d9f28a0ca85418bd6ffde89ed67
Margarida <margaridammcm@gmail.com>
2022-09-04 18:59:02 -0300

    Add return to function validar

--- api/rotas/fornecedores/fornecedor.js
+++ api/rotas/fornecedores/fornecedor.js
@@ -1,11 +1,10 @@
 // representar no codigo o Fornecedor
-const tabelaFornecedor = require('./tabelaFornecedor')
 const TabelaFornecedor = require('./tabelaFornecedor')
 const CampoInvalido = require('../../erros/campoInvalido')
 const DadosNaoFornecidos = require('../../erros/dadosNaoFornecidos')
 
 class Fornecedor {
-  // pegar dados na hora de instanciar a classe e assimilar a nossa instancia temos que criar o metodo que constroi a classe
+  // pegar dados na hora de instanciar a classe e assimilar a nossa instancia temos que criar o metodo que constroi a classe  
   constructor ({ id, empresa, email, categoria, dataCriacao, dataAtualizacao, versao }) {
     this.id = id
     this.empresa = empresa
@@ -35,18 +34,19 @@ class Fornecedor {
     this.empresa = fornecedorEncontrado.empresa // com os dados que recebemos do banco de dados precisamos pegar os valores e assimilar a nossa instancia para conseguirmos responder a requisicao
     this.email = fornecedorEncontrado.email
     this.categoria = fornecedorEncontrado.categoria
-    this.dataCriacao = fornecedorEncontrado.categoria
+    this.dataCriacao = fornecedorEncontrado.dataCriacao
     this.dataAtualizacao = fornecedorEncontrado.dataAtualizacao
     this.versao = fornecedorEncontrado.versao
   }
 
   async atualizar () { // assincrona para o node esperar esse metodo terminar antes de seguir e usarmos a nossa funcao
-    await tabelaFornecedor.pegarPorId(this.id) // saber que o fornecedor existe
+    await TabelaFornecedor.pegarPorId(this.id) // saber que o fornecedor existe
     const campos = ['empresa', 'email', 'categoria'] // pegar valores e montar um objeto para mandarmos para o banco //algumas propriedades como dataCriacao, dataAtualizacao, versao e id nao atualizamos, a propria tabela pelo mysql atualiza
     const dadosParaAtualizar = {} // dentro desse objeto colocaremos o que queremos atualizar
 
     campos.forEach((campo) => { // verificar se cada campo desses foi fornecido e está válido
       const valor = this[campo] // dados que recebemos para cada campo
+
       if (typeof valor === 'string' && valor.length > 0) {
         dadosParaAtualizar[campo] = valor
       }
@@ -56,13 +56,13 @@ class Fornecedor {
       throw new DadosNaoFornecidos()
     }
 
-    await tabelaFornecedor.atualizar(this.id, dadosParaAtualizar) // como vai retornar uma promessa pq vai se comunicar com o banco de dados, colocamos um await
+    await TabelaFornecedor.atualizar(this.id, dadosParaAtualizar) // como vai retornar uma promessa pq vai se comunicar com o banco de dados, colocamos um await
   }
 
   remover () {
-    return tabelaFornecedor.remover(this.id)
+    return TabelaFornecedor.remover(this.id)
   }
-
+  
   // adicionar validacao para rota de post para poder criar um fornecedor
   // estamos recebendo dados e criando no banco de dados mas nao estamos fazendo uma validacao se os dados sao validos para conseguirmos cadastrar dados ou impedir que dados invalidos sejam enviados para nossa tabela
   validar () {
@@ -73,6 +73,7 @@ class Fornecedor {
         throw new CampoInvalido(campo)
       }
     })
+    return true
   }
 }
 
