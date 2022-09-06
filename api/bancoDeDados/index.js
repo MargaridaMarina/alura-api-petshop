02b6de1736d1160061dd95ced9336b82481298e5
Margarida <margaridammcm@gmail.com>
2022-09-05 23:05:00 -0300

    correct index

--- api/bancoDeDados/index.js
+++ api/bancoDeDados/index.js
@@ -1,17 +1,14 @@
-// exportar conexao com banco de dados
+const Sequelize = require('sequelize')
+const config = require('config')
 
-const Sequelize = require('sequelize') // biblioteca Sequelize para se comunicar com banco de dados
-const config = require('config') // consumir dados do arquivo de config default.json
-const instancia = new Sequelize( // configurar dados de acesso ao banco de dados
-  // config.get('mysql.banco-de-dados'),
+const instancia = new Sequelize(
+  config.get('mysql.banco-de-dados'),
   config.get('mysql.usuario'),
   config.get('mysql.senha'),
   {
-    // configuracoes da conexao
-    host: config.get('mysql.host'), // ip da maquina que ta rodando o banco mysql
-    dialect: 'mysql' // tipo de banco que estamos trabalhando
-    // port: config.get('mysql.port')
+    host: config.get('mysql.host'),
+    dialect: 'mysql'
   }
 )
 
-module.exports = instancia // exportar para conseguir utilizar em outros arquivos
+module.exports = instancia
