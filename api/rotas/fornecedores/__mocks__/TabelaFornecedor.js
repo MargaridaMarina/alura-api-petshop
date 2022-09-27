91cc7f374740385b34ff90c400ada8e024beaafa
Margarida <margaridammcm@gmail.com>
2022-09-27 10:06:10 -0300

    include info

--- api/rotas/fornecedores/__mocks__/TabelaFornecedor.js
+++ api/rotas/fornecedores/__mocks__/TabelaFornecedor.js
@@ -2,7 +2,6 @@ module.exports = {
   listar () {
     return []
   },
-
   inserir (fornecedor) {
     return {
       id: 500,
@@ -11,13 +10,16 @@ module.exports = {
       versao: 90
     }
   },
-
   async pegarPorId (id) {
+    return {
+      id: 500,
+      dataCriacao: '10/12/3420',
+      dataAtualizacao: '10/12/3420',
+      versao: 90
+    }
   },
-
   async atualizar (id, dadosParaAtualizar) {
   },
-
   async remover (id) {
   }
 }
